5282784eb93658c31a6eed950c5a5e3558308217
bak88 <dimbak1990@gmail.com>
2025-01-22 19:50:50 +0300

    PZ/1

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        printThreeWords();
+    }
+
+    public static void printThreeWords(){
+        System.out.println("Orange\n" + "Banana\n" + "Apple");
+    }
+
+
+
+
+
+
+}
